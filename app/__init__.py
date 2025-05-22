5fbf40309200d2d9e972ad6b30c5abde2fe85b70
Taine <tcsmith2@waimea.school.nz>
2025-05-22 12:23:52 +1200

    errormessage&form

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import redirect
 from random import randint
 
 # Create the app
@@ -15,7 +16,22 @@ def random():
     randNum = randint(1, 1000)
     return render_template('pages/random.jinja', number=randNum ) 
 
+@app .get("/number/<int:num>")
+def analyseNumber(num):
+    print(f"You entered: {num}")
+    return render_template('pages/number.jinja', number=num ) 
 
 @app .get("/about/")
 def about():
     return render_template('pages/about.jinja')
+
+@app .get("/form")
+def form():
+    return render_template('pages/form.jinja')
+
+
+
+@app.errorhandler(404)
+def notFound(e):
+    return render_template('pages/home.jinja')
+
